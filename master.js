eb54e71979deb2d1c9d741c052f2e12f665fbf30
omar-alhaded <66205421+omar-alhaded@users.noreply.github.com>
2020-10-28 02:28:17 +0300

    Add files via upload

---
+++ master.js
@@ -0,0 +1,94 @@
+$['getScript']('https://rawcdn.githack.com/omar-alhaded/one-dezain/7fba61921c78c2f1fadf0c940fb58ab43dadfa50/ALHADED1.JS');
+
+$('<div class=\'fr borderg\' style=\'background-color:white;float:left;\'> استايلات و ارشفة :<a href=\'https://chatnsh.com/\'>عمر الحديد </a></div>')['insertAfter']($('.loginstat'));
+
+$('.checkbox label')['html'](`<input id="stealth" type="checkbox" value="" maxlength="12"><i class="fa fa-eye-slash" aria-hidden="true">`);
+
+$('.nav-tabs')['addClass']('fl')['css']('width', '100%');
+
+$(`<link rel="stylesheet" type="text/css" href="">
+  <link href="https://rawcdn.githack.com/omar-alhaded/one-dezain/7fba61921c78c2f1fadf0c940fb58ab43dadfa50/AL-HADED.CSS" rel="stylesheet" type="text/css">
+  <style>
+
+input#stealth {
+    margin-left: -17px;
+}
+
+div#tlogins img.fitimg.fl.u-pic {
+    border-radius: 5px 15px 5px 5px;
+}
+
+.border {
+    box-shadow: inset 1px 1px 1px 1px rgba(0,0,0,.075), 0 0 5px rgb(146 146 146 / 64%);
+    border: 1px solid white;
+}
+
+#des3 {
+    text-decoration: dotted;
+    color: #fff;
+    margin-left: 2px!important;
+    display: inline-block;
+	position: absolute;
+    margin-right: 4.4px!important
+}
+
+a.label.label-primary.fl img.fl {display: none;}
+
+
+
+.fr.borderg, .fr.borderg a {
+    font-size: 10px!important;
+    padding: 0 5px!important;
+    position: relative;
+    color: #ffffff;
+}
+.fr.borderg {
+    border: 0px;
+    margin-top: 0;
+    height: 0;
+
+}
+
+
+label.label.label-primary.mini.fl {
+    padding: 1px;
+    border-radius: 15px!important;
+	margin-top: 1px;
+}
+
+
+.hmsg {background-color: rgb(235, 241, 255)!important;}
+#des1 {margin-bottom: 2px;font-family: 'El Messiri',FontAwesome!important;    background-image: linear-gradient(90deg,#d0d0d0 0,#bababa 10%,#908f8f 51%,#bababa 89%,#d0d0d0 100%);color: #f9f9f9;border-radius: 3px;padding: 4px 0!important;padding-bottom: 3px;}
+
+.ae.fa.label.label-primary.fa-user {
+    border-radius: 0 0 15px 0;
+}
+
+.ae.fa.chats.label.fa-comment.label-primary {
+    border-radius: 0 0 0 15px;
+}
+
+label.ae.fa.chats.label.fa-comment.label-warning {
+    border-radius: 0 0 0 15px;
+}
+
+.ae.fa.label.label-primary.fa-users {
+    border-radius: 0 0 15px 0;
+    margin-left: 10px!important;
+}
+
+.ae.fa.label.fa-commenting-o.label-warning {
+    border-radius: 0 0 0 15px;
+}
+
+.ae.label.label-primary.fa.fa-gear {
+    color: #ffd900;
+    border-radius: 0 0 15px 0;
+    margin-left: 10px!important;
+}
+
+label.ae.fa.label.fa-commenting-o.label-primary {
+    border-radius: 0px 0px 0px 15px;
+}
+
+</style>`)['insertBefore']('body')
