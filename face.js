ad3d65d5372e4cfa8dbe348010df1a78213f0ff0
omar-alhaded <66205421+omar-alhaded@users.noreply.github.com>
2021-02-12 10:26:27 +0200

    Create face.js

---
+++ face.js
@@ -0,0 +1,239 @@
+$(`<a id="عمر_الحديد" href=""><i class="fa fa-refresh fa-spin fa-3x fa-fw" aria-hidden="true"></i></a>
+<img id="asim70" src="https://chatnsh.com/sico/z1euakuf5o10.jpg" style="width:100%">
+<a id="asim27" target="_blank" type="a" rel="nofollow" title="" href="" class="label-primary">Programming by OMAR ALHADED</a></div></div><center>
+`).insertBefore('.nav-tabs');
+
+$('.nav-tabs').addClass('fl').css('width','100%');
+
+
+$(`
+<center>
+     <div id="asim1"> 
+      <a id="asim7" rel="dofollow" title="القوانين" href="" style="border-radius: 0 4px 0 0; border-right: 2px solid#f7d322;" class="fl mini  fa fa-bullhorn btn btn-primary" target="_blank"></a> 
+      <a id="asim7" rel="dofollow" title="الاشتراكات" href="" style="border-right: 1px solid#fff;" class="fl mini  fa fa-star-half-o btn btn-primary" target="_blank"></a> 
+      <a id="asim7" rel="dofollow" title="راديو" href="" style="border-right: 1px solid#fff;" class="fl mini  fa fa-volume-up btn btn-primary" target="_blank"></a> 
+      <a id="asim7" rel="dofollow" title="زخرفة اسماء" href="" style="border-right: 1px solid#fff;" class="fl mini  fa fa-fire btn btn-primary" target="_blank"></a> 
+      <a id="asim7" rel="dofollow" title="اتصل بنا" href="" style="border-right: 1px solid#fff;" class="fl mini fa fa-paper-plane btn btn-primary" target="_blank"></a> 
+      <a id="asim7" rel="dofollow" title="قوانين السوابر" href="" style="border-right: 1px solid#fff;" class="fl mini fa fa-exclamation-triangle btn btn-primary" target="_blank"></a> 
+      <a id="asim7" rel="dofollow" title="تطبيق الشات" href="" style="border-right: 1px solid#fff;" class="fl mini fa fa-android btn btn-primary" target="_blank"></a> 
+      <a id="asim7" rel="dofollow" title="اختصارات الشات" href="" style="border-radius: 6px 0 0 0; border-left: 3px solid#f7d322; border-right: 1px solid#ffffff;" class="fl mini fa fa-tencent-weibo btn btn-primary" target="_blank"></a> 
+     </div>
+     <center>`).insertBefore('div#tlogins .lonline.light.break');
+
+$(`
+<center>
+     <div>
+      <marquee direction="right" id="as43" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="3">
+        &nbsp;أهلا وسهلا بكم في شات 
+       <font color="#595757" class="asim12">آحبـك</font> ● · ‫أفضل سوبر لهذا الأسبوع · ● 
+       <font class="asim11" color="#727A54">&nbsp; قـــريــــبـــآ &nbsp;</font> ● · ‫أفضل زائر لهذا الأسبوع · ● 
+       <font class="asim11" color="#ff9900">&nbsp;قـــريــــبـــآ&nbsp;</font> ● · ‫أفضل زائرة لهذا الأسبوع · ● 
+       <font class="asim11" color="#ff00ff">&nbsp; قـــريــــبـــآ &nbsp;</font> ● · مبدع الحائط لهذا الأسبوع · ● 
+       <font class="asim11" color="#BA5D82">&nbsp; قـــريــــبـــآ &nbsp;</font> ● · مبدعة الحائط لهذا الأسبوع · ● 
+       <font class="asim11" color="#800000">&nbsp; قـــريــــبـــآ&nbsp;</font> ● · ‫أفضل تواجد لهذا الأسبوع · ● 
+       <font class="asim11" color="#B55B7F">&nbsp; قـــريــــبـــآ&nbsp;</font> ● · مع تمنياتنا للجميع بـ أطيب الاوقات&nbsp;
+      </marquee>
+     </div><center>
+`).insertBefore('div#tlogins .lonline.light.break');
+
+$(`<link rel="stylesheet" href="https://rawcdn.githack.com/omar-alhaded/one-dezain/f00f9c8fde115ac9369b7549040baba3d3637117/face.css">
+`).insertBefore('body');
+
+$('.ae.label.label-primary.fa.fa-gear')['html'](``);
+
+
+$('label.ae.fa.label.label-primary.fa-commenting-o')['html'](``);
+
+
+$('.ae.fa.label.label-primary.fa-users')['html'](``);
+
+
+$('.hand.nosel.fl.uzr.border')['css']({
+    margin: '1px 0'
+    , borderRadius: '0px'
+})
+
+$('.checkbox label')['html'](`${''}${'<input id="stealth"type="checkbox"value=""><i class="fa fa-eye-slash"aria-hidden="true"style="margin-top: 3px"></i>'}${''}`)
+
+$(`<a href="" target="_blank" id="as0" class="border">تطبيق الشات</a>
+`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg');
+
+
+var nopm = !0
+    , nonot = !0;
+
+
+$('textarea#tbox')['bind']('contextmenu', function (_0x5493x6) {
+    _0x5493x6['preventDefault']()
+}, false);
+
+
+
+$('textarea#tbox')['on']('paste', function (_0x5493x6) {
+    _0x5493x6['preventDefault']()
+});
+
+
+
+$('div#l1 button.btn.btn-primary')['bind']('click', function () {
+    alert('اهلا وسهلا بكم في شات آحبـك ❤️ سجل عضوية تحصل على 5000 لايك وزخرفة :: طريقة التسجيل اضغط على خيار (تسجيل) اكتب (اكتب اسمك المستعار + كلمة المرور ) وبعدها اضغط دخول')
+});
+
+
+
+$(`<style>
+.hand.corner{background-color:#000!important;border:2px solid #c12e2a!important;color:#f0ad4e;border-radius:0;text-align:center;border-style:solid;border-right:7px solid!important;border-left:7px solid!important;border-color:#2ea1ad;border-left-style:double!important;border-right-style:double!important}div.break.fl::before{content:" اخي الزائر اختي الزائرة اذا واجهت مشكله في دخول الشات يرجى مراسلتنا من صفحة اتصل بنا 📨 .. ";color:#0f0!important;font-family:'jazeera',FontAwesome;font-size:16px}.break.fl{text-align:center;padding:5px!important;font-family:'jazeera',FontAwesome;color:red;-webkit-animation:mymove .5s infinite;animation:mymove .5s infinite}
+</style>
+`).insertBefore('div#tlogins .lonline.light.break');
+
+
+$(`<img tabindex="0" class="al120 fl " style="margin-left: -3px; padding:6px; width:35px;margin-top: -2px;" src="https://k.top4top.io/p_17044g8ns1.gif" data-original-title="" title="">
+`).insertBefore('img.fl.nosel.emobox');
+
+
+$('.al120')['click'](function () {
+    alert(' اللهم أحفـظ { ‘العــرب‘ } من كل بـلآء - تحيآتي للجميع ')
+});
+
+
+
+$('div#l2 button.btn.btn-primary')['bind'];
+$('div#l2 button.btn.btn-primary')['bind']('click', function () {
+    alert(' اهلا  وسهلا بكم مجددآ ب شات احبـك   ❤ ')
+});
+
+
+
+
+$('div#l3 button.btn.btn-primary')['bind']('click', function () {
+    alert(' ❤  ممنوع علي الجميع ذكر السياسات او سب الذات الالهية او ذكر  اسم مواقع وعدم مذايقة الزوار واتمنا من الجميع الالتزام ب القواعد والقوانين  الخاصة  ب شات ❤')
+})
+
+$(".fa-gear").text("الضبط").css("width","");
+
+$(".ubnr").remove();
+
+$('.u-msg.dots').removeClass('dots');
+
+
+if(location.protocol!='https:'){location.protocol='https:';}
+
+
+
+/*
+
+if(location.protocol!='https:'){location.protocol='https:';}
+$(`<img src="https://chatnsh.com/sico/z1etc745gq10.jpg" class="fr" style="width:100%;margin-top: 0px;">
+`).insertBefore('.nav-tabs');
+$(`<a id="des3" href="#" style="border: 1px solid white;border-radius: 0px 15px 0px 0px!important;width:70px;margin:-145px 2px 2px;" class="fl mini  fa   fa-phone btn btn-warning"> الأداره </a>
+<a id="des3" href="#" style="border: 1px solid white;border-radius: 0px!important;width:70px;margin:-116px 2px 2px;" class="fl mini  fa  fa-paint-brush btn btn-danger"> بنرات </a>
+<a id="des3" href="#" style="border: 1px solid white;border-radius: 0px!important;width:70px;margin:-87px 2px 2px;" class="fl mini  fa   fa-star btn btn-success"> اشتراك </a>
+<a id="des3" href="#" style="border: 1px solid white;border-radius: 0px!important;width:70px;margin:-58px 2px 2px;" class="fl mini  fa   fa-image btn btn-warning"> رمزيات </a>
+<a id="des3" href="#" style="border: 1px solid white;    border-radius: 0px 0px 15px 0px!important;width:70px;margin:-29px 2px 2px;" class="fl mini  fa fa-heartbeat btn btn-info"> شات </a>
+`).insertBefore('.nav-tabs');
+$.getScript("https://cdn.statically.io/gh/waleed-qaid/1des/efd657f6/www.1des.net-chatt-www.nbd-oman.com0.js");
+$('.nav-tabs').addClass('fl').css('width','100%');
+$(`<link rel="stylesheet" type="text/css" href="">
+<link rel="stylesheet" href="">
+<style>
+button.btn {
+    border-radius: 5px!important;
+}
+.fr.co, div#tlogins img.fr.co {
+    margin-top: 10px!important;
+}
+</style>
+`).insertBefore('body');
+$('.break.light').css({"background-image":"url()"});
+*/
+$(` <center><div width="99%" id="asim7"><font style="background-color: #dbf6f1;border-radius: 15px 15px 15px 15px;font-family: 'jazeera',FontAwesome;padding: 2px 10px 2px 10px;margin: 7px 10px 7px 10px;" > مبدع الحائط   (قـريبا )_❥   </font></div><center>`).insertBefore('div#d2bc');
+/*
+$(`<div><marquee direction="right" width="100%" id="des0" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="4">    اهلا وسهلا بكم في شات <font color="#850D0D" class="des2">  آحبـك  للجوال </font>افضل اداري لهذا الاسبوع<font class="des2" color="#800000">   قــريبا </font>افضل مراقب لهذا الاسبوع<font class="des2" color="#ff9900"> قــريبا </font>افضل تواجد لهذا الاسبوع<font class="des2" color="#ff00ff">   قــريبا     </font>مبدع الحائط لهذا الاسبوع<font class="des2" color="#5CAD9A">    قــريبا </font>مبدعة الحائط لهذا الاسبوع<font class="des2" color="#800000">      َقــريبا  </font>مع تمنياتنا للجميع باطيب الاوقات </marquee></div>`).insertBefore('div#l1');
+$('.u-msg.dots').removeClass('dots');
+$(".ubnr").remove();
+$('.u-msg.dots').removeClass('dots');
+$(".fa-gear").text("الضبط").css("width","");
+*/
+$("div#tlogins input").attr("maxlength",10)
+sendpic_=sendpic;
+reqlikes=100;
+sendpic=function(){if(getuser(myid).rep<reqlikes){alert('لإيمكنك تغير صورتك يتطلب منك الحصول على '+reqlikes+'لايكات'); return;}else{sendpic_();}}
+setprofile_=setprofile;
+reqlikes=100;
+setprofile=function(){if(getuser(myid).rep<reqlikes){alert('لا يمكنك حفظ الأعدادات يتطلب منك الحصول على '+reqlikes+'لايكات'); return;}else{setprofile_();}}
+    Tsend_=Tsend;
+reqlikes=100;
+Tsend=function(){if(getuser(myid).rep<reqlikes){alert('لإيمكنك الكتابة بالعام يتطلب منك الحصول على '+reqlikes+'لايكات'); return;}else{Tsend_();}}
+
+/*
+$('div#l1 button').attr('onclick', '');
+$('div#l1 button').click(function(){
+alert('دخول الزوار مغلق الرجاء تسجيل عضويه لدخول شات قــريبا');
+});
+vchat=true;
+$('.dpnl').append(`
+<div id="mic" style="height: 100%;width:100%;" class="break light tab-pane border"> 
+<audio class="wp-audio-shortcode" id="audio-236-1" preload="none" style="width: 100%;" controls="controls"><source type="audio/mpeg" src="https://j.top4top.io/m_1592gehs81.mp3"><a href="https://j.top4top.io/m_1592gehs81.mp3">https://j.top4top.io/m_1592gehs81.mp3</a></audio>
+</div>`);
+$('#d0').append(`<label title="القرآن الكريم" href="#" onclick="$('.pnhead').text($(this).attr('title'));hl($(this),'primary');setTimeout(function(){$('#mic').scrollTop(0);},100);$('.dpnl').show();"
+data-toggle="tab" data-target="#mic"  class="ae fa label label-primary fa-microphone ">.</label>`);
+
+
+
+*/
+
+$('.ae.label.label-primary.fa.fa-gear')['html'](`${'⠀'}`);
+$('.ae.fa.label.label-primary.fa-users')['html'](`${''}`);
+var nopm = !0
+    , nonot = !0;
+/*
+$('#d0')['append'](`${'
+<label title="الجدار" href="#" onclick="$(\'.pnhead\').text($(this).attr(\'title\'));$(\'.dpnl\').show();setTimeout(function(){$(\'#d2bc\').scrollTop(0);},100);hl($(this),\'primary\');$(this).find(\'.bwall\').text(\'0\');bcc=0" data-toggle="tab" data-target="#wall" class="ae fa label label-primary fa-commenting-o"><span class="bwall minix badge border" style="margin-top:-8px;padding:1px 10px">0</span></label>
+'}`)
+*/
+
+
+$('#d0')['append'](`<label title="غرف الدردشه" href="#" onclick="$('.pnhead').text($(this).attr('title'));$('.dpnl').show();" data-toggle="tab" data-target="#rooms" class="ae fa label label-primary fa-users">⠀</label>`)
+$('#d0')['append'](`
+<label id="asim22" title="الجدار" href="#" onclick="$('.pnhead').text($(this).attr('title'));$('.dpnl').show();setTimeout(function(){$('#d2bc').scrollTop(0);},100);hl($(this),'primary');$(this).find('.bwall').text('0');bcc=0" data-toggle="tab" data-target="#wall" class="ae fa label fa-commenting-o label-primary"><span class="bwall minix badge border" style="margin-top:-8px;padding:1px 4px">0</span></label>`);
+
+
+$('#d0').append(`
+<label title="تصميم الصور اون لاين" href="#" onclick="$('.pnhead').text($(this).attr('title'));
+hl($(this),'primary');
+setTimeout(function()
+	
+	{
+		$('#mic').scrollTop(0);
+	
+}
+,0);
+$('.dpnl').show();
+" data-toggle="tab" data-target="#mic" class="ae fa label fa-volume-up label-primary">&nbsp;</label>`);
+$('.dpnl').append(`
+<div id="mic" style="height: 100%;width:100%;" class="break light tab-pane border active"><center></center><center>
+<center><a class="fl mini fa fa-bullhorn  btn btnn-default">&nbsp; محرر ومعدل الصور بـ احترافيه للجميع</a>
+<iframe type="text/html" id="jfa200" width="85%" height="100%" src="https://www.photoeditoangel.com/photo-editor/pixie/" scrolling="no" frameborder="0"></iframe>
+<iframe type="text/html" id="jfa150" width="100%" height="100px" src="https://reach-audio.esteam.rocks/public/nogoum_fm/embed" scrolling="no" frameborder="0"></iframe>
+<img id="jfa180" border="0" src="https://mrkzgulfup.com/uploads/160655645727691.jpg">
+<a id="jfa160" target="_blank" type="a" href="https://t.me/ihtc0" style="border-radius: 0 0 0 10%; border-left: 2px solid#a6a6a6; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;" class="btn minix btn-primary">أتصل بنأء</a>
+<center></center></center></center></div>`);
+
+
+  document.getElementById('tbox')
+
+    .placeholder = 'عمي عوف السطح ودردش ويانا'
+    , $('.fl.ustat')
+    .css('width', '5px')
+    , $('.fa.fa-user-plus')
+    .html(`تسجيل`)
+    , document.getElementById('u1')
+    .placeholder = 'اكتب اسمك يزاير.اشرب جاي يزاير'
+    , document.getElementById('pass1')
+    .placeholder = 'باسووردك'
+    , document.getElementById('u2')
+    .placeholder = 'اسم عضويتك'
+    , document.getElementById('pass2')
+    .placeholder = 'باسووردك بس لتنسى'
+    , document.getElementById('u3')
+    .placeholder = 'اسمك لازم تحفظه';
