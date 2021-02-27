2e4381337e2f1cda28ca1dab210f4c7fd6416bdd
omar-alhaded <66205421+omar-alhaded@users.noreply.github.com>
2021-02-27 22:44:05 +0200

    Create speed-host.js

---
+++ speed-host.js
@@ -0,0 +1,38 @@
+if(location.protocol!='https:'){location.protocol='https:';}
+$(`
+    <center>
+    <div id="asim13">
+<a id="asim21" href=""style="width: 62px;padding:0;height: 20px;" class="btn minix btn-primary">اتصل بنا</a>
+<a id="asim21" href="" style="width: 62px;padding:0;height: 20px;" class="btn minix btn-primary">التطبيق</a>
+<a id="asim21" href=""style="width: 62px;padding:0;height: 20px;" class="btn minix btn-primary">القوانين</a>
+<a id="asim21" href=""style="width: 62px;padding:0;height: 20px;" class="btn minix btn-primary">اشتراكات</a>
+<a id="asim21" href=""style="width: 85px;padding:0;height: 20px;" class="btn minix btn-primary">دردشة احبك </a>
+<a id="asim21" href=""style="width: 50px;padding:0;height: 20px;" class="btn minix btn-primary">راديو</a></div></div>
+<center>
+`).insertBefore(".nav-tabs"),
+$(".nav-tabs").addClass("fl").css("width","100%")
+$(".hand.nosel.fl.uzr.border").css({margin:"1px 0",borderRadius:"0px"})
+$('.checkbox label').html(`<input id="stealth" type="checkbox" value=""><i class="fa fa-eye-slash" aria-hidden="true"></i>`)
+$(".fa-gear").text("الضبط").css("width","");
+$(`<label onclick="window.open('cp?token='+token,'_blank');" style="background-color: ghostwhite;color: black;margin:4px; padding:8px;width:98%;" class="label tc border cp  btn  label-danger fl"><span class="fl fa fa-star"></span>ادارة بيانات المواقع</label>`).insertAfter('#settings label.label.tc.border.btn.fl[onclick="sendpic();"]');
+$('.u-msg.dots').removeClass('dots');
+$(".ubnr").remove();
+$(`<a href="" target="_blank" style="width:107px;color: #E8415D;text-decoration:none; margin-right: 5px;margin-left: 2px;text-align: center;float: right;border-radius: 5px;border-color: #F2F2F2;padding: 3px;" class="border" ><i class="fa fa-android"></i> التطبيق</a>
+`).insertBefore(".umute")
+$(`<style>
+    .fr.co,.fl.mini.u-co{display:none}.uzr.fl.corner.borderg.hmsg.mm{background-color:#fff!important;-webkit-text-fill-color:#40a4ff}.border{border:1px solid #e8e8e8}.ae{border:1px solid #e4e4e4!important;border-radius:3px;margin:0}span.border{border:none}.label-primary,.btn-primary,.bg-primary,.label-primary:hover,.btn-primary:hover,.btn-primary:focus,.label-primary,.btn-primary{border:1px solid #fff}.tbox{padding:5px;min-height:32px}input#u1,input#u2,input#pass1,input#u3,input#pass2{width:74%;height:29px}ul.nav.nav-tabs>li>a{color:#fff;border:none;text-align:center;background-color:#088795;border-radius:3px;display:initial;width:80%;float:left;padding:0!important}ul.nav.nav-tabs>li.active>a{color:#fff;background-color:#216A72;border:none;border-radius:3px}ul.nav.nav-tabs>li{width:32%;margin:0 5px 0 0}ul.nav.nav-tabs>li>.fa-user-plus:before,ul.nav.nav-tabs>li>.fa-user:before,ul.nav.nav-tabs>li>.fa-user:before{float:right;margin-right:-26%;border:1px solid;padding:0 2px 1px;border-radius:3px;width:23px;text-align:center;background-color:#fff;color:#088795}.nav>li>a:focus,.nav>li>a:hover{color:#9abff9}ul.nav.nav-tabs{background-color:#f5fdff!important;border-top:1px solid #ddd;margin-top:1px;float:right;width:100%;padding:3px 1px}input#u1,input#u2,input#pass1,input#u3,input#pass2{width:61%;height:31px;margin-left:40.5px;margin-bottom:5.5px;border:1px solid #94adc1;border-radius:3px;text-align:center}div#asim13{width:57%;top:120px;position:absolute;left:-8px}div#l1{padding:16.8px!important}.emoi{width:auto;max-width:-webkit-fill-available}.btn.hand.borderg.corner{width:25px;height:25px}.u-msg.break.fl{line-height:26px!important}body.bg.center-block.bg.dad div#tlogins{width:auto!important;height:auto!important}div#l3,div#l2,div#l1{margin-bottom:24px}.popover.right{margin-left:-84px;max-width:305px;width:308px}label.label.label-primary.mini.fl{border-top:1px dotted #fff!important;top:295px;position:absolute;width:66%!important;height:22px;padding:4px 4px 4px 170px;border-radius:0!important;right:-2px;font-size:14px!important;border:1px solid #fff0}span.s1.fa.fa-user.label.badgex.label-as-badge.label-success{height:19px!important;font-size:14px!important;border:0 solid;padding:3px 0 0;border-radius:3px;width:37px;text-align:center;background-color:#50749500;color:#fff;position:absolute;top:0;left:135px}label.fl.label.loginstat.label-success{border-top:1px dotted #fff!important;height:22.1px;top:295px;position:absolute;left:-8px;width:15%;border-radius:0!important;margin-left:2px!important}.fr.borderg{border-top:1px dotted #fff!important;margin-right:0!important;top:295px;background-color:#216A72!important;border:none;float:left;left:50px;position:absolute;width:81.4%!important;height:22px;border-radius:0!important;border:1px solid #fff0}.fr.borderg,.fr.borderg a{font-size:12px!important;padding:0 30px 0 4px !important;color:#fff}#as43{top:295px;position:absolute;width:37%!important;height:22px;padding:1px 0 0;border-radius:25px!important;right:120px;font-size:12px!important;color:#f9f9f9;border-left:6px solid #fff;border-right:6px solid #f8f8f8}.checkbox{top:220px;position:absolute!important;right:70px}a.label.label-primary.fl{display:none}.fa-user-times{margin:0!important;background-color:#FFF;top:0!important;margin-left:-368px!important;transform:rotate(-90deg);width:123px!important;margin-top:-240px!important}.ukick{margin:0!important;width:60px!important;background-color:#FFF!important;margin-top:14px!important;margin-left:-60px!important;position:absolute;top:1px}.uban{margin:0!important;width:60px!important;background-color:#FFF;margin-top:73px!important;margin-left:-61px!important;position:absolute;top:1px}.btn.hand.borderg.corner{width:25px;height:25px}.emoi{width:auto;max-width:-webkit-fill-available}.uzr.fl.corner.borderg.hmsg.mm{background-color:#fff!important}.bgg.border.corner{right:1px}.callnot{position:absolute!important;top:1px!important;margin-left:0!important;left:1px}.pmsgc{background-color: #eeeeee5e!important; -webkit-text-fill-color: #4200e2;}.btn.btn-primary.dots.roomh.border.corner{background-color:#000!important}
+</style>`).insertBefore("body");
+$(`<div><marquee direction="right" id="as43"scrollamount="3">آهلا وسهلا بكم في دردشة احبك <font class="a"> ● ·</font>   نورت وطنك هنا نتعرف ع بعض تحت سماء دردشة  يعمل 
+ع جميع الجوالات واجهز
+ة الكمبيوتر حيث انه شات الجوال ودردشة الجوال والحاسوب مع دردشتي انضم الينا الآن ·● مع تمنياتنا للجميع بأطيب الاوقات </marquee><div>`).insertBefore('div#tlogins .lonline.light.break');
+$(".nav-tabs").addClass("fl").css("width","100%");
+$(`<style> 
+    .btn-dang { background-image: -webkit-linear-gradient(top,#c7c7c7 0,#000000 100%)!important; background-image: -o-linear-gradient(top,#d9534f 0,#c12e2a 100%)!important; background-image: -webkit-gradient(linear,left top,left bottom,from(#000000),to(#c7c7c7))!important; background-image: linear-gradient(to bottom,#c7c7c7 0,#000000 100%)!important; filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#ffd9534f', endColorstr='#ffc12e2a', GradientType=0)!important; filter: progid:DXImageTransform.Microsoft.gradient(enabled=false); background-repeat: repeat-x; border-color: #c7c7c7!important; } 
+</style> `).insertBefore('head');
+
+Tsend_=Tsend;
+reqlikes=50;
+Tsend=function(){if(getuser(myid).rep<reqlikes){alert('لإ يمكنك الكتابة بالعام يتطلب منك الحصول على لايكات'+reqlikes+' ');return;}else{Tsend_();}}
+setprofile_=setprofile;
+reqlikes=50;
+setprofile=function(){if(getuser(myid).rep<reqlikes){alert('لا يمكنك حفظ الأعدادات يتطلب منك الحصول على لايكات '+reqlikes+'');return;}else{setprofile_();}}
