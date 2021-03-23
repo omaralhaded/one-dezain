71a7cf7440a693c0523063c9b42ac472c0430c3d
omar-alhaded <66205421+omar-alhaded@users.noreply.github.com>
2021-03-23 20:26:07 +0200

    Add files via upload

---
+++ cod.js
@@ -0,0 +1,104 @@
+$(`<a id="ادارة-جفى" href=""><i class="fa fa-refresh fa-spin fa-3x fa-fw" aria-hidden="true"></i></a><img id="jfa1"src="sico/z1f1g3f9cg10.jpg"style="width:100%"><a id="jfa2"target="_blank"type="a"rel="nofollow"title="ادارة-شات-جفى"href="" class="label-primary"style="margin:-2px 0 0 -374px"></a></div></div><center>
+`)['insertBefore']('.nav-tabs')
+$('.nav-tabs')['addClass']('fl')['css']('width', '100%')
+$(`<center><div id="jfa3">
+<a id="jfa4" href="#" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-fahd">اتصل بنا</a> 
+<a id="jfa4" href="#" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-fahd">التطبيق</a> 
+<a id="jfa4" href="#" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-fahd">وظائف شاغره</a> 
+<a id="jfa4" href="#" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-fahd">اختصارات</a> 
+<a id="jfa5" href="#" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-fahd">الأبراج</a>
+<a id="jfa22" href="https://t.me/ihtc0" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-fahd">الأداره</a> 
+     </div>
+     <center>`)['insertBefore']('div#tlogins .lonline.light.break');
+
+$(`<center>
+<div><marquee direction="right" id="jfa01" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="3"> &nbsp;أهلا وسهلا بكم في شات <font color="#595757" class="jfa001">آحبــك للجـوأل </font> ● · ‫أفضل سوبر لهذا الأسبوع · ● <font class="jfa002" color="#727A54">&nbsp قريــبآ &nbsp;</font> ● · ‫أفضل زائر لهذا الأسبوع · ● <font class="jfa002" color="#ff9900">&nbsp;قريــبآ&nbsp;</font> ● · ‫أفضل زائرة لهذا الأسبوع · ● <font class="jfa002" color="#ff00ff">&nbsp;قريباَ^_^&nbsp;</font> ● · مبدع الحائط لهذا الأسبوع · ● <font class="jfa002" color="#5CAD9A">&nbsp; قريباَ^_^ &nbsp;</font> ● · مبدعة الحائط لهذا الأسبوع · ● <font class="jfa002" color="#800000">&nbsp; قريباَ^_^ &nbsp;</font> ● · ‫أفضل تواجد لهذا الأسبوع · ● <font class="jfa002" color="#417570">&nbsp; قريباَ^_^ &nbsp;</font> ● · مع تمنياتنا للجميع بـ أطيب الاوقات&nbsp;
+</marquee></div><center></center>`)['insertBefore']('div#tlogins .lonline.light.break');
+
+$(`<link rel="stylesheet" href="https://rawcdn.githack.com/omar-alhaded/one-dezain/512a99d54e2fa04848612272c42c87433626c5a3/cod.css">\x0D\x0A<style>iframe#fahd-jfa { float: left; width: 26px; height: 26px; position: absolute; left: 67px; } img#fahd4 { padding: 1px 0 0; width: 54px; margin-top: 0; margin-left: -17px; margin-right: -8px; } span.busers.minix.badge.border,span.s1.fa.fa-user.label.badgex.label-as-badge.label-success,a#asim27{ background-color: hsla(0, 0%, 100%, 0) !important; border: hsla(0, 0%, 100%, 0) !important; }</style>
+`)['insertBefore']('body');
+
+$('.ae.label.label-primary.fa.fa-gear')['html'](``);
+
+$('label.ae.fa.label.label-primary.fa-commenting-o')['html']('<span class="bwall  minix badge border" style="margin-top:-8px;padding:1px 4px;">0</span>');
+
+$('.ae.fa.label.label-primary.fa-users')['html'](``);
+
+$('.hand.nosel.fl.uzr.border')['css']({margin: '1px 0'
+    , borderRadius: '0px'
+        })
+$('.checkbox label')['html'](`<input id="stealth" type="checkbox" value=""><img id="as13" src="sico/z1f1g2mvsj10.gif" width="80%" style=""><img id="as12" src="sico/z1f1g2n05s10.gif" width="60%" style="\x0D\x0A    margin-left:-227px;margin-top:-8.0px;position:absolute;width:60%;height:176%;float:left;\x0D\x0A    ">`);
+
+$(`<a href="#" target="_blank" id="as0" class="border">تطبيق الشات</a>
+`)['insertBefore']('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg')
+
+var nopm = !0
+    , nonot = !0;
+
+$('#d0')['append'](`<label title="غرف الدردشه" href="#" onclick="$('.pnhead').text($(this).attr('title'));$('.dpnl').show();" data-toggle="tab" data-target="#rooms" class="ae fa label label-primary fa-users"><span class="brooms minix  badge border" style="margin-top:-8px;padding:1px 4px;">9</span></label>`);
+
+$('#d0')['append'](`<label title="الإعدادات" href="#" onclick="$('.pnhead').text($(this).attr('title'));$('.dpnl').show();" data-toggle="tab" data-target="#settings" style="width: 46px;" class="ae label label-primary fa fa-gear">&nbsp;</label>`);
+$('#d0')['append'](`<label title="رموز لـ الزخرفه "href="#"onclick="$(\'.pnhead\').text($(this).attr(\'title\'));hl($(this),\'primary\');setTimeout(function(){$(\'#mic\').scrollTop(0);},100);$(\'.dpnl\').show()"data-toggle="tab"data-target="#mic"class="asim ae fa label label-primary"><i class="fa fa-fire"aria-hidden="true"></i></label>`);
+
+
+$('div#tlogins button.btn.btn-primary')['click'](function ()
+    {setTimeout(function (){refreshonline()}, 0)}
+    )
+setInterval(function (){var a = $('#dpnl');
+0 < a['length'] && a['remove']()}, 0)
+$('.dpnl')['append'](`<div id="mic"style="height: 100%;width:100%;"class="break light tab-pane border"><center></div><center></div>`)
+$('div#tlogins button.btn.btn-primary')['click'](function (){
+var b = setInterval(function (){var a = $('.pmsgc')['length'];if (a > 0){
+                    
+$(`
+<textarea class="jfa999" style="height:190px;overflow:hidden">⠀ ⠀ ⠀ ⠀ ⠀ ⠀ ⠀ ⠀ 
+♔ ★  ║ 彡  ♫ • ❥ ✿  ⌯ ✘ ●┊♡♰❰ ❱ ᵛ͢ᵎᵖ  ₎™ 🦅🌹💀☠️👑🌝✨💥🔥🍂☂ ❥ ‏˛⁽ ツ‏⠀ ᵀᴹ )"̯ء 
+♪
+♤ ♧ ♡ ♢ ♚ ♛ ★ ✪ ✯ ✰ ☻ ☺ ☄ ☾ ☼
+℃ ℉ ° ❅ ϟ ☦ ✞ ☥ ⌘ ✡ ۞ ✄ ✆ ✉ ∞
+❤ ❥ ❦ ❧ ✔ ✘ ▲ ▼ ◆ ◎ ☚ Δ ◕ ◔ Ω
+
+</textarea>
+<iframe type="text/html" id="jfa200" width="100%" height="90%" src="https://www.photoeditoangel.com/photo-editor/pixie/" scrolling="no" frameborder="0"></iframe>`)['appendTo']('#mic');
+                    
+clearInterval(b)} else{console['log'](a)}}, 2000)});
+$('textarea#tbox')['bind']('contextmenu', function (a){a['preventDefault']()}, false);
+
+$('textarea#tbox')['on']('paste', function (a){a['preventDefault']()});
+
+    
+
+$(`<a class="fl mini fa fa-bullhorn  btn btnn-default">&nbsp; لـ الشكاوي .والاقتراحات . </a>
+<input type="jfa-w" name="FirstName" value="( @ihtc0 ) اتصال بناء: عبر تليجرام"><center>`)['appendTo']('#settings')
+
+
+$('div#l1 button.btn.btn-primary')['bind']('click', function (){alert('اهلا وسهلا بكم في شات احبك للجوال  ❤️ سجل عضوية تحصل على 5000 لايك وزخرفة :: طريقة التسجيل اضغط على خيار (تـسجيـل )  (اكتب اسمك المستعار + كلمة المرور ) وبعدها اضغط دخول')});
+
+$(`<style>.hand.corner{background-color:#000!important;border:2px solid #c12e2a!important;color:#f0ad4e;border-radius:0;text-align:center;border-style:solid;border-right:7px solid!important;border-left:7px solid!important;border-color:#2ea1ad;border-left-style:double!important;border-right-style:double!important}div.break.fl::before{content:" اخي الزائر اختي الزائرة اذا واجهت مشكله في دخول الشات يرجى مراسلتنا من صفحة اتصل بنا ✉️ .. ";color:#0f0!important;font-family:\'jazeera\',FontAwesome;font-size:16px}.break.fl{text-align:center;padding:5px!important;font-family:\'jazeera\',FontAwesome;color:red;-webkit-animation:mymove .5s infinite;animation:mymove .5s infinite}</style>`)['insertBefore']('div#tlogins .lonline.light.break');
+
+$(`<img tabindex="0" class="al120 fl " style="margin-left: -21px; padding:6px; width:42px;margin-top: -2px;" src="sico/z1f1g2mvsj10.gif" data-original-title="" title="">`)['insertAfter']('img.fl.nosel.emobox');
+
+$('div#tlogins button.btn.btn-primary')['click'](function ()
+{var b = setInterval(function (){
+var a = $('.pmsgc')['length'];
+0 < a ? ($(`<iframe src="https://raw.githack.com/jfa-w/new/master/fahad-jff8a.html" width="23px" height="23px" scrolling="no" frameborder="no" marginheight="0" marginwidth="0" style="float: left;width: 23px;margin-top: 2.4px;height: 23px;position: absolute;left: 65px;border-radius: 100%;"></iframe>
+`)['insertAfter']('button.fa.fa-sign-out.fl.btn.btn-primary'), clearInterval(b)) : console['log'](a)}, 1)})
+$('div#tlogins button.btn.btn-primary')['click'](function (){
+        setTimeout(function (){
+                refreshonline()
+                }, 0)});
+
+$(`<img tabindex="0" class="jfa fl " style="padding:5px;width: 55px;margin-top: -6px;margin-left: -17px;margin-right: -8px;"data-original-title="" title="">
+`)['insertAfter']('img.fl.nosel.emobox')
+$(".ubnr").remove();
+$('.u-msg.dots').removeClass('dots');
+$("div#tlogins input").attr("maxlength",10)
+sendpic_=sendpic;
+reqlikes=100;
+sendpic=function(){if(getuser(myid).rep<reqlikes){alert('لإيمكنك تغير صورتك يتطلب منك الحصول على '+reqlikes+'لايكات'); return;}else{sendpic_();}}
+setprofile_=setprofile;
+reqlikes=100;
+setprofile=function(){if(getuser(myid).rep<reqlikes){alert('لا يمكنك حفظ الأعدادات يتطلب منك الحصول على '+reqlikes+'لايكات'); return;}else{setprofile_();}}
+    Tsend_=Tsend;
+reqlikes=100;
+Tsend=function(){if(getuser(myid).rep<reqlikes){alert('لإيمكنك الكتابة بالعام يتطلب منك الحصول على '+reqlikes+'لايكات'); return;}else{Tsend_();}}
