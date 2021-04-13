0be3047d7ac64f23967e19bb801601e171d370eb
omar alhaded <59962403+omaralhaded@users.noreply.github.com>
2021-04-13 18:09:52 +0300

    Add files via upload

---
+++ masr.js
@@ -0,0 +1,221 @@
+$(`<div class="desadmins"><button class="desadmin">صوم مقبول</button></div></div>`).insertBefore('.nav-tabs')
+
+$(`<img src="https://chatnsh.com/sico/z1f35qvgkp10.png" style="position: absolute;transform: matrix(1, 0, 0, 1, 0, 28);right: 15px;background-color: #ffffff00;" class=".lonline.light.break">`).insertBefore('.loginstat')
+$(`<img src="https://chatnsh.com/sico/z1f35qvh0h10.png" style="transform: matrix(1, 0, 0, 1, 0, 145);margin-top: 25px;right: 24px;position: absolute;" class=".lonline.light.break">`).insertBefore('.loginstat')
+$('<div class="fr borderg" style="padding:2px;background-color:white;float:left;margin-left:4px;"> استايلات و ارشفة :<a href="">عمر الحديد</a></div>')['insertAfter']($('.loginstat'));
+$(`<link rel="stylesheet" type="text/css" href="">
+<link rel="stylesheet" href="">
+<style>
+div#tlogins .hand.nosel.fl.uzr.border {
+    background-image: linear-gradient(90deg ,#210505 0,#21050540 10%,#2105051a 51%,#fff 89%,#fff 100%)
+}
+
+.desadmin {
+    background-color: #048978
+}
+
+.dessite {
+    background-color: #a74b56
+}
+
+.btn-primary,.label-primary {
+    background-color: #210505!important
+}
+
+.fr.borderg,.fr.borderg a {
+    border-radius: 3px!important;
+    margin-right: 0!important;
+    border: 0 solid #000;
+    color: #fff
+}
+
+.bg {
+    background-color: #270b0b
+}
+
+.light {
+    background-color: #f3f3f3
+}
+
+div#tlogins ::-webkit-scrollbar {
+    width: 5px;
+    height: 0
+}
+
+div#tlogins ::-webkit-scrollbar-button {
+    width: 5px;
+    height: 5px
+}
+
+div#tlogins ::-webkit-scrollbar-thumb {
+    background: #210505;
+    border: 71px #fff;
+    border-radius: 31px
+}
+
+div#tlogins ::-webkit-scrollbar-thumb:active {
+    background: 0 0
+}
+
+div#tlogins ::-webkit-scrollbar-track {
+    background: #fff;
+    border: 65px #fff;
+    border-radius: 34px
+}
+
+div#tlogins ::-webkit-scrollbar-track:hover {
+    background: 0 0
+}
+
+div#tlogins ::-webkit-scrollbar-track:active {
+    background: 0 0
+}
+
+div#tlogins ::-webkit-scrollbar-corner {
+    background: 0 0
+}
+
+div#tlogins .fl.mini.u-msg {
+    margin-top: 3px!important;
+    border-radius: 3px!important
+}
+
+div#tlogins .u-topic.dots {
+    width: 800px
+}
+
+
+
+
+
+
+a.label.label-primary.fl {border-radius: 3px!important;margin: 2px -152px;padding: 5px!important;margin-bottom: 2px;text-align: right!important;}
+
+
+
+
+
+
+
+
+.desadmin {
+    color: white;
+    font-size: 16px;
+    border: none;
+    margin-left: 154px;
+    margin-top: 2px;
+    height: 25px;
+    width: 148px;
+    border-radius: 3px;
+
+}
+
+
+
+
+
+input[type="jfa-w"] {
+    margin-left: 60px;
+    text-align: center;
+    border-radius: 15px;
+}
+button.btn.fr.btn-success.fa.fa-refresh{display:none}
+a.label.label-primary.fl img.fl{display:none}
+a.label.label-primary.fl{
+border-radius:3px!important;
+padding:5px!important;
+margin-bottom:2px;
+}
+#des22{text-align:center!important;font-size:100%!important}
+label.fl.label.loginstat{background-color:transparent!important;position:absolute;top:0;margin-top:4px;left:0}
+.fr.borderg,.fr.borderg a{font-size:10px!important;padding:0 5px!important;position:relative;color:#ffffff}
+.fr.borderg{border:0;margin-top:0;height:0}
+span.s1.fa.fa-user.label.badgex.label-as-badge.label-success{padding:0 3px 2px;margin-right:5px}
+label.label.label-primary.mini.fl{padding:1px;border-radius:3px!important}
+div#l1 input#u1,div#l3 button{width:99%;margin-left:1px}
+div#l2 button,div#l3 button{border:1px solid #ccc!important;border-radius:24px!important;height:27px;padding:0 5px}
+div#l1 input#u1{border-radius:15px;border:1px solid #e1e0e0;margin-top:3px;text-align:center}
+div#l2 input#pass1,div#l2 input#u2,div#l3 input#pass2,div#l3 input#u3{width:49%;padding:0!important;float:right;margin:2px 5px 2px 0}
+div#l2 input#u2,div#l3 input#u3{margin-left:-6px;border-radius:15px;border:1px solid #e1e0e0;text-align:center}
+div#l2 input#pass1,div#l3 input#pass2{border-radius:15px;border:1px solid #e1e0e0;text-align:center}
+div#l2,div#l3{padding:2px!important}div#l1{margin:0 1px -18px;padding:0!important}
+div#l1,div#l2,div#l3{margin-bottom:-8px;border-top:2px solid;margin-top:0;border-color:#bababa;border-radius:15px;height:69px}
+div#l1 button{border:1px solid #ccc!important;border-radius:24px!important;width:99%;height:27px;padding:0 5px}
+div#l2 button{width:70%}
+ul.nav.nav-tabs>li>a{border:none;text-align:center;border-radius:3px;display:initial;width:80%;float:left;padding:0!important}
+ul.nav.nav-tabs>li.active>a{border:none;border-radius:3px}
+ul.nav.nav-tabs>li{width:32%;margin:0 5px 0 0}
+ul.nav.nav-tabs>li>.fa-user-plus:before,ul.nav.nav-tabs>li>.fa-user:before,ul.nav.nav-tabs>li>.fa-user:before{float:right;margin-right:-26%;padding:0 2px 1px;border-radius:3px;width:23px;text-align:center}
+.nav>li>a:focus,.nav>li>a:hover{color:#9abff9}
+ul.nav.nav-tabs{margin-top:2px;float:right;width:100%;padding:3px 1px}
+a.label.label-primary.fl img.fl,button.btn.fr.btn-success.fa.fa-refresh,div#l2 br,div#l3 br{display:none}
+
+
+
+div#tlogins .hand.nosel.fl.uzr.border {
+    width: 99% !important;
+    margin-top: 2px !important;
+border: 1px solid #c3c2c2;
+}
+
+div#tlogins .hand.nosel.fl.uzr.border {
+    box-shadow: inset 1px 1px 1px 0 #ffffff, 0 0 2px #ffffff00;
+}
+
+.border {
+    border: 0px solid black;
+}
+
+div#tlogins img.fitimg.fl.u-pic {
+    border-radius: 5px;
+    box-shadow: 0px 0px 3px 0px rgb(0 0 0 / 43%);
+    border: 1px solid #6f6f6f;
+	width: 44px!important;
+    height: 44px!important;
+}
+
+
+div#tlogins .fl.mini.u-msg {
+    margin-top: -6px!important;
+    border-radius: 3px!important;
+}
+
+
+#des0 {
+    transform: matrix(1, 0, 0, 1, 158, -127);
+    border-radius: 15px 15px 15px 15px;
+    position: absolute;
+    width: 69% !important;
+    height: 19px;
+    left: -50px;
+    font-size: 14px !important;
+    color: #fff;
+    padding: 0px 0 !important;
+    background-color: #141414;
+}
+
+
+.des2 {
+    background-color: #fff;
+    border-radius: 0;
+    padding: 2px 10px;
+    margin: 3px 8px!important;
+	border-radius: 15px!important;
+}
+</style>`).insertBefore('body')
+
+var nopm = !0;
+  var nonot = !0;
+$(".ubnr").remove();
+$('.u-msg.dots').removeClass('dots');
+$(".fa-gear").text("الضبط").css("width","");
+$("div#tlogins input").attr("maxlength",10)
+sendpic_=sendpic;
+reqlikes=100;
+sendpic=function(){if(getuser(myid).rep<reqlikes){alert('لإيمكنك تغير صورتك يتطلب منك الحصول على '+reqlikes+'لايكات'); return;}else{sendpic_();}}
+setprofile_=setprofile;
+reqlikes=100;
+setprofile=function(){if(getuser(myid).rep<reqlikes){alert('لا يمكنك حفظ الأعدادات يتطلب منك الحصول على '+reqlikes+'لايكات'); return;}else{setprofile_();}}
+    Tsend_=Tsend;
+reqlikes=100;
+Tsend=function(){if(getuser(myid).rep<reqlikes){alert('لإيمكنك الكتابة بالعام يتطلب منك الحصول على '+reqlikes+'لايكات'); return;}else{Tsend_();}}
